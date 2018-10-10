21d517104a05e03c27df93949cc2c94203d626da
TGazica <tomislav.gazica@gmail.com>
2018-10-10 22:29:42 +0200

    edited retrofit util

--- app/src/main/java/org/tomislavgazica/weatherapp/networking/RetrofitUtil.java
+++ app/src/main/java/org/tomislavgazica/weatherapp/networking/RetrofitUtil.java
@@ -2,17 +2,28 @@ package org.tomislavgazica.weatherapp.networking;
 
 import org.tomislavgazica.weatherapp.util.Constants;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitUtil {
 
-    // okhttp klijnr, interceptor za logcat
-
-    public static Retrofit createRetrofit() {
+        public static Retrofit createRetrofit() {
         return new Retrofit.Builder()
                 .baseUrl(Constants.WEATHER_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient())
                 .build();
+    }
+
+    private static HttpLoggingInterceptor provideLoggingInterceptor() {
+        return new
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    }
+
+    private static OkHttpClient okHttpClient() {
+        return new
+                OkHttpClient.Builder().addInterceptor(provideLoggingInterceptor()).build();
     }
 }
